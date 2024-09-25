77b7937cc8764d6d04762ffa3b29e3c29e7a338a
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-09-25 07:21:08 +0000

    add files

---
+++ Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const name ="rajsvi"
+let age = 25
+var pass = "12345" 
+city ="Malegaon"
+let gender
+
+//name="xyz" //not allowed
+//console.log(name);
+age = 20
+pass = "7890"
+city = "pune"
+console.table([name,age,pass, city]);
---
+++ Basics/01_dataTypes.js
@@ -0,0 +1,11 @@
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined =>empty
+// symbol => unique
+
+//object =>
+
+    console.log(typeof 12345);
