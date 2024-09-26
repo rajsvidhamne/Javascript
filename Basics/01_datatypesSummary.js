57417f5bf3085c959a02e4e238a3207305256cdd
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-09-26 10:00:18 +0000

    add summary

---
+++ Basics/01_datatypesSummary.js
@@ -0,0 +1,11 @@
+//primitive 
+//7 types: number, string, boolean,null, undefined, symbol, bigInt
+
+
+const score = 100
+const scoreValue = 100.5
+
+const isLoggedIn = false
+
+//non-primitive (reference)
+//array,objects,function
