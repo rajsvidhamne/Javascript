32c86c0e048a02f2549b591cf65db00648ff2c62
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-09-28 14:05:24 +0000

    commit changes

--- Basics/03_dates.js
+++ Basics/03_dates.js
@@ -1,7 +1,29 @@
 //Dates
 
-let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+//let myCreadtedDate = new Date(2024, 0 , 28)
+//let myCreadtedDate = new Date(2024, 0 , 28,5,3)
+//let myCreadtedDate = new Date("09-29-2024")
+//console.log(myCreadtedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreadtedDate.getTime());
+// console.log(Date.now()/1000);
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+//`${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
