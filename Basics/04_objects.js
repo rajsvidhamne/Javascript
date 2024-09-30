704d6eedccd89247536a9851fc45cc6cc90799cd
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-09-30 08:01:02 +0000

    object

---
+++ Basics/04_objects.js
@@ -0,0 +1,17 @@
+//object
+//singleton 
+//object literals
+
+const JsUser = {
+    name: "rajsvi",
+    "full name": "rajsvi dhamne",
+    age: 18,
+    location: "jaipur",
+    email: "rajsvi@gmail.com",
+    isLoggedIn: false,
+    LastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
