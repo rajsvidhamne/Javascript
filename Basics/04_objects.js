b12a1e997d1d57ab7acafb884c2753f4ad68e4b0
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-09-30 10:07:16 +0000

    add

--- Basics/04_objects.js
+++ Basics/04_objects.js
@@ -2,9 +2,12 @@
 //singleton 
 //object literals
 
+
+const mySym = Symbol ("key1")
 const JsUser = {
     name: "rajsvi",
     "full name": "rajsvi dhamne",
+    [mySym]: "mykey1",
     age: 18,
     location: "jaipur",
     email: "rajsvi@gmail.com",
@@ -12,6 +15,27 @@ const JsUser = {
     LastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser.[mySym]);
 
+// JsUser.email = "rajsvi@google.com"
+// //Object.freeze(JsUser)
+// JsUser.email = "rajsvi@microsoft.com"
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
