ef9d96723a28ba4c61e1ea6eea9d338895d4d6b4
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-10-01 09:23:43 +0000

    execution

---
+++ Basics/05_execution.js
@@ -0,0 +1,9 @@
+//there are 3 types ofjs execution context
+//1.global execution context
+//2. function execution context
+//3. eval execution context
+
+
+//2 types of phases
+//1.memory creation phase
+//2.execution phase
