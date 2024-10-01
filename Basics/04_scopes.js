dba8f1f0b68425be49c7a4da12f34f6ca1d55857
rajsvidhamne <118924545+rajsvidhamne@users.noreply.github.com>
2024-10-01 06:01:12 +0000

    complete

--- Basics/04_scopes.js
+++ Basics/04_scopes.js
@@ -4,10 +4,46 @@
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER:", a);
+    //console.log("INNER:", a);
  }
 
  
- console.log(a);
+ //console.log(a);
  //console.log(b);
  //console.log(c);
+
+
+ function one(){
+    const username = "rajsvi";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    //console.log(website);
+    two(); 
+}
+
+
+
+ if (true) {
+    const username = "rajsvi";
+    if (username === "rajsvi") {
+        const website1 = " youtube"
+       // console.log(username + website1);   
+    }
+    //console.log(website1);  
+}
+//console.log(username);
+
+ //********************intresting******************* */
+ console.log (addone(5))
+ function addone(num) {
+    return num + 1
+ } 
+
+
+ addtwo(5)
+ const addtwo = function(num){
+    return num + 2
+ }
